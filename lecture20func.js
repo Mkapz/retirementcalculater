cbeffd5fc861010791c475e0f9c7587afbe565d6
Michael Kapronica <mkap25@yahoo.com>
2019-01-25 00:04:52 -0500

    practice using functions, not tested yet

---
+++ lecture20func.js
@@ -0,0 +1,28 @@
+//function nameoffunction(argument)
+
+function calculateAge(birthYear){
+	return 2018 - birthYear;
+}
+
+var ageJohn= calculateAge(1990);
+var ageMike= calculateAge(1948);
+var ageJane= calculateAge(1982);
+console.log(ageJohn. ageMike. ageJane);
+
+function yearUntilRetirement(year, firstName)
+{
+	var age= calculateAge(year);
+	var retirement= 65 - age;
+	
+	if( retirement > 0){
+		console.log(firstName + ' retires in ' +
+			retirement + ' years.');
+	}else{
+		console.log(firstName + 'is already retired.')
+	}
+}
+
+
+yearUntilRetirement(1990, 'John');
+yearUntilRetirement(1948, 'Mike');
+yearUntilRetirement(1982, 'Jane');
